cafa1f5c337b6bd1c3669184bd8f6dd53a651bb1
ThugBunny <52386312+ThugBunny@users.noreply.github.com>
2019-08-26 20:31:50 -0600

    Update class.ts

--- class.ts
+++ class.ts
@@ -1,9 +1,14 @@
 class Shape {
   area: number;
-  color: string;
+  private color: string;
   
-  constructor (name: string, width:number, height: number){
+  constructor (public name: string, width:number, height: number){
     this.area = width * height;
     this.color = "pink";
   }
+  
+  shoutout(){
+    return "I'm " + this.name + " with an area of " + this.area + "cm square";  
+  }
+  
 };
