fd97e1c65a7845a69c3f5c4ace956b7c02203dbf
ThugBunny <52386312+ThugBunny@users.noreply.github.com>
2019-08-26 20:36:33 -0600

    Update class.ts

--- class.ts
+++ class.ts
@@ -12,3 +12,25 @@ class Shape {
   }
   
 };
+
+class Shape3D extends Shape {
+  volume: number;
+  
+  constructor ( public name: string, width: number; height: number, length: number){
+    super( name, width, height );
+    this.volumne = length * this.area;
+  }
+  
+  shoutout() {
+    return "I'm " + this.name + " with a volume of " + this.volume +" cm cube.";  
+  }
+  
+  superShout() {
+    return super.shoutout(); 
+  }
+}
+
+var cube = new Shape3D("cube", 30, 30, 30);
+console.log( cube.shoutout() );
+console.log( cube.superShout() );
+
