7e8380a5fb8618cca5e1a223591b537581df23bd
ThugBunny <52386312+ThugBunny@users.noreply.github.com>
2019-08-28 00:40:04 -0600

    Update clases.ts

--- clases.ts
+++ clases.ts
@@ -1,15 +1,29 @@
 class Person {
-  name: string;
-  constructor(theName: string) {
+  private name: string;
+  protected age: number;
+  protected constructor(theName: string, theAge: number) {
     this.name = theName;
+    this.age = theAge;
   }
   introduceSelf() {
     console.log("Hi, I am " + this.name + "!");
   }
 }
-let personA = new Person("Sally");
-// Muestra "Hi, I am Sally!"
-personA.introduceSelf();
-personA.name = "Mindy";
-// muestra "Hi, I am Mindy!"
-personA.introduceSelf();
+class Friend extends Person {
+  yearsKnown: number;
+  constructor(name: string, age: number, yearsKnown: number) {
+    super(name, age);
+    this.yearsKnown = yearsKnown;
+  }
+  timeKnown() {
+    console.log("We have been friends for " + this.yearsKnown + " years.")
+  }
+  friendSince() {
+    let firstAge = this.age - this.yearsKnown;
+    console.log(`We have been friends since I was ${firstAge} years old.`)
+  }
+}
+let friendA = new Friend("William", 19, 8);
+// imprime: We have been friends since I was 11 years old.
+friendA.friendSince();
+
