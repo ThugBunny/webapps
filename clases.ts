040cd8112128a88b4472b4b4ac9b90009fc27668
ThugBunny <52386312+ThugBunny@users.noreply.github.com>
2019-08-28 00:35:06 -0600

    Update clases.ts

--- clases.ts
+++ clases.ts
@@ -1,11 +1,15 @@
 class Person {
-name: string;
-constructor(theName: string) {
-this.name = theName;
-}
-introduceSelf() {
-console.log("Hi, I am " + this.name + "!");
-}
+  name: string;
+  constructor(theName: string) {
+    this.name = theName;
+  }
+  introduceSelf() {
+    console.log("Hi, I am " + this.name + "!");
+  }
 }
 let personA = new Person("Sally");
+// Muestra "Hi, I am Sally!"
+personA.introduceSelf();
+personA.name = "Mindy";
+// muestra "Hi, I am Mindy!"
 personA.introduceSelf();
