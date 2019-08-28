c77f6da45355ff8968f0451a9be20a157fb3ebc9
ThugBunny <52386312+ThugBunny@users.noreply.github.com>
2019-08-28 00:54:14 -0600

    Update genericos.ts

--- genericos.ts
+++ genericos.ts
@@ -1,9 +1,20 @@
-function removeChar(theString: string, theChar: string): string {
-  let theRegex = new RegExp(theChar, "gi");
-  return theString.replace(theRegex, '');
+interface People {
+  name: string
 }
+interface Family {
+  name: string,
+  age: number,
+  relation: string
+}
+interface Celebrity extends People {
+  profession: string
+}
+function printName<T extends People>(theInput: T): void {
+  console.log(`My name is ${theInput.name}`);
+}
+let serena: Celebrity = {
+  name: 'Serena Williams',
+  profession: 'Tennis Player'
+}
+printName(serena);
 
-function removeIt<T>(theInput: T, theIt: string): T {
-  let theRegex = new RegExp(theIt, "gi");
-  return theInput.replace(theRegex, '');
-}
