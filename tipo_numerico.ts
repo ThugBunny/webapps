e2441fdebe3c5c02fe45c5a2258dc3c61506ca30
ThugBunny <52386312+ThugBunny@users.noreply.github.com>
2019-08-27 19:52:28 -0600

    Create tipo_numerico.ts

---
+++ tipo_numerico.ts
@@ -0,0 +1,18 @@
+// Con strictNullChecks en true
+let a: number = undefined;  //Error
+let b: number = null;       //Error
+let c: number = 3;
+let d: number = 0b111001; //Binary
+let e: number = 0o436;    //Octal
+let f: number = 0xadf0d;  //HEcadecimal
+let g: number = "cat";  //Error
+
+// Con strictNullChecks en true
+let a: number = undefined;  //Error
+let b: number = null;       //Error
+let c: number = 3;
+let d: number = 0b111001; //Binary
+let e: number = 0o436;    //Octal
+let f: number = 0xadf0d;  //HEcadecimal
+let g: number = "cat";  //Error
+
